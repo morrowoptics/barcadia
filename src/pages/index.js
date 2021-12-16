e9635e43b72f6dc6d26b3b1353e3835e95826311
morrowoptics <66558958+morrowoptics@users.noreply.github.com>
2021-12-17 00:43:42 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const Index = () => {
       <Seo title="Home" />
       <Layout>
         <BannerModule
-          title="Barcadia"
+          title="Barcjlkjljadia"
           subTitle="A product based template system for GatsbyJS"
         />
         <BasicTextModule />
